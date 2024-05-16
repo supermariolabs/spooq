9770968057303721a5cc743a663ef308fd9db1c5
antom90 <antonio.monciino@me.com>
2024-05-16 11:57:43 +0200

    update README.md

--- src/main/scala/com/github/supermariolabs/spooq/etl/RestApiStep.scala
+++ src/main/scala/com/github/supermariolabs/spooq/etl/RestApiStep.scala
@@ -28,17 +28,12 @@ class RestApiStep extends CustomInputStep {
       optionDefined.filterKeys(!_.startsWith("api_rest_")) foreach (option =>
         reader.option(option._1, option._2))
     })
-    /*customInputStep.options.getOrElse(Map.empty).foreach(optionEntry => {
-      logger.info(s"Options: ${optionEntry._1} type:\n${optionEntry._2}")
-    })*/
 
     val options = customInputStep.options.getOrElse(Map.empty)
-    //options("api_rest_authentication_body")
     customInputStep.format match {
       case Some(format) =>
         if (customInputStep.format.get == "json") {
           val httpClient: CloseableHttpClient = HttpClients.createDefault()
-          //authenticate(options, httpClient)
           options.get("api_rest_method") match {
             case Some(method) =>
               customInputStep.path.map(p => {
@@ -75,7 +70,6 @@ class RestApiStep extends CustomInputStep {
                     } else throw new IllegalArgumentException(s"Error during authentication with code: ${authResponse.getStatusLine.getStatusCode} \n cause: $result")
                   } else logger.info("No authentication specified in the config file. Doing request without authentication...")
                   request.addHeader("Content-Type", "application/json")
-                  //request.addHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImtXYmthYTZxczh3c1RuQndpaU5ZT2hIYm5BdyIsImtpZCI6ImtXYmthYTZxczh3c1RuQndpaU5ZT2hIYm5BdyJ9.eyJhdWQiOiJodHRwczovL2lyZW5iZWUub25taWNyb3NvZnQuY29tL0ZlZWRpbmdCZWUiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9iOGM2MTdlZC0wN2EyLTQ2NzktYTdlMi0yODQ1YzYwNzk1NDIvIiwiaWF0IjoxNzA4NDI4OTMzLCJuYmYiOjE3MDg0Mjg5MzMsImV4cCI6MTcwODQzMjgzMywiYWlvIjoiRTJOZ1lEQnBMTDJlK0tZNThiaW9vZDFpaVlLNUFBPT0iLCJhcHBpZCI6ImNhNjA3OTE2LTkxMTYtNDNhZS1iZjk4LWFmNDQ4M2RkMWIwMSIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0L2I4YzYxN2VkLTA3YTItNDY3OS1hN2UyLTI4NDVjNjA3OTU0Mi8iLCJvaWQiOiJhY2ZmMzFkYi02MDM0LTQ4NmEtYmU1OS03NjQwZTFiZmIyZGQiLCJyaCI6IjAuQVNBQTdSZkd1S0lIZVVhbjRpaEZ4Z2VWUXQwNWliLXpVS0pEdGNWYXhrSmVrelRrQUFBLiIsInN1YiI6ImFjZmYzMWRiLTYwMzQtNDg2YS1iZTU5LTc2NDBlMWJmYjJkZCIsInRpZCI6ImI4YzYxN2VkLTA3YTItNDY3OS1hN2UyLTI4NDVjNjA3OTU0MiIsInV0aSI6IjhRamdDUDdJc2ttdG51cTNvN2t4QUEiLCJ2ZXIiOiIxLjAifQ.grZLUYQv6E50I4y93n-lHAixGkJA7sH5EsyfVFoKv7UFLCIZhULgcuhJ7rTAj-M_kj8y0UnUxwDLP1mgUcvtas4ptASxfc-I7RwMFypCLBD9YruA10-JUytLTZAl0O2NapitdSb6ZxhwfcMsfk5jf4SgKZuna3aEH-Wpj6oBZaEHldTRtuSvCRAXoMvxCkBXfnrbW0kdEhPdp71NqrDF60n59Cl7ACdW4UZAIgzjB8BqfMom3lvwkd4bpQYxaGwT0QYo3aaWUUD9f6Fx8SX1Hot_bX6JsGKMWA9hMaALGbIcp3dy_0qQMBxAMQvY0_xNlCY5YEvXJP0UD_RJllEuqQ")
                   val response = httpClient.execute(request)
                   response.getStatusLine.getStatusCode match {
                     case 200 =>
@@ -83,9 +77,6 @@ class RestApiStep extends CustomInputStep {
                       val data = EntityUtils.toString(entity)
                       httpClient.close()
                       val stringDS = spark.createDataset(data.split("\n"))(Encoders.STRING)
-                      /*spark.read
-                        .option("multiline", "true")
-                        .json(stringDS)*/
                       reader.json(stringDS)
                     case 401 | 403 =>
                       httpClient.close()
